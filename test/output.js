9046ac0dcdc3f36408cb57dd826f25f768e0b888
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-30 19:36:17 +0200

    Update tests

--- test/output.js
+++ test/output.js
@@ -2,9 +2,11 @@
 
 const test = require('ava')
 const { count, resume } = require('../bin/view/render')
+const { dropRight } = require('lodash')
 
 test('render count', t => {
   const state = {
+    startTimestamp: 0,
     count: {
       '2xx': 3,
       '5xx': 4,
@@ -13,11 +15,13 @@ test('render count', t => {
     fetchingUrl: 'https://facebook.com'
   }
 
-  t.snapshot(count(state))
+  const result = dropRight(count(state).split('\n')).join('\n')
+  t.snapshot(result)
 })
 
 test('render links', t => {
   const state = {
+    startTimestamp: 0,
     count: {
       '2xx': 3,
       '5xx': 4,
@@ -25,26 +29,27 @@ test('render links', t => {
     },
     links: {
       '2xx': [
-        [200, 'https://google.com/1'],
-        [200, 'https://google.com/2'],
-        [200, 'https://google.com/3']
+        [200, 'https://google.com/1', 0],
+        [200, 'https://google.com/2', 0],
+        [200, 'https://google.com/3', 0]
       ],
       '5xx': [
-        [500, 'https://google.com/4'],
-        [501, 'https://google.com/5'],
-        [501, 'https://google.com/6'],
-        [501, 'https://google.com/7'],
-        [501, 'https://google.com/8']
+        [500, 'https://google.com/4', 0],
+        [501, 'https://google.com/5', 0],
+        [501, 'https://google.com/6', 0],
+        [501, 'https://google.com/7', 0],
+        [501, 'https://google.com/8', 0]
       ],
       '4xx': [
-        [404, 'https://google.com/9'],
-        [404, 'https://google.com/10'],
-        [403, 'https://google.com/11'],
-        [403, 'https://google.com/12'],
-        [405, 'https://google.com/13']
+        [404, 'https://google.com/9', 0],
+        [404, 'https://google.com/10', 0],
+        [403, 'https://google.com/11', 0],
+        [403, 'https://google.com/12', 0],
+        [405, 'https://google.com/13', 0]
       ]
     }
   }
 
-  t.snapshot(resume(state))
+  const result = dropRight(resume(state).split('\n')).join('\n')
+  t.snapshot(result)
 })
