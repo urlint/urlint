291d0fd87118aca1ee09ffb057ce28847652e11b
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-04-30 19:24:02 +0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -1,17 +1,26 @@
 'use strict'
 
+const { isNumber, isString, forEach } = require('lodash')
 const pEvent = require('p-event')
 const test = require('ava')
 
 const urlint = require('..')
 
 test('Resolve an array of urls', async t => {
-  const urls = [
-    'https://kikobeats.com',
-    'https://microlink.io'
-  ]
+  const urls = ['https://kikobeats.com', 'https://microlink.io']
 
   const emitter = await urlint(urls)
   const data = await pEvent(emitter, 'end')
-  t.snapshot(data)
+
+  forEach(data, groupByStatusCode => {
+    forEach(
+      groupByStatusCode,
+      ({ statusCodeGroup, statusCode, url, timestamp }) => {
+        t.is(true, isString(statusCodeGroup))
+        t.is(true, isNumber(statusCode))
+        t.is(true, isString(url))
+        t.is(true, isNumber(timestamp))
+      }
+    )
+  })
 })
