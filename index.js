5a9a3178527eb677498162424195078412b7fb7a
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-02 13:42:59 +0200

    follow redirects support

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 'use strict'
 
-const getStatusCode = require('url-code-status')
+const reachableUrl = require('reachable-url')
+const isRedirect = require('is-redirect')
+const dnsErrors = require('dnserrors')
 const timeSpan = require('time-span')
+const pick = require('lodash.pick')
 const aigle = require('aigle')
 const mitt = require('mitt')
 
+const RESPONSE_PROPS = ['redirectUrls', 'url', 'requestUrl', 'statusCode']
+
 const getStatusByGroup = statusCode => `${String(statusCode).charAt(0)}xx`
+
+const getLastError = aggregatedError => {
+  const errors = Array.from(aggregatedError)
+  const error = errors[errors.length - 1]
+  return error
+}
+
+const fetch = async (targetUrl, opts) => {
+  let res
+  let timestamp
+  let statusCode
+
+  try {
+    timestamp = timeSpan()
+    res = await reachableUrl(targetUrl, { ...opts, followRedirect: false })
+    timestamp = timestamp()
+    statusCode = res.statusCode
+    if (!isRedirect(statusCode)) { return { ...pick(res, RESPONSE_PROPS), timestamp } }
+
+    timestamp = timeSpan()
+    res = await reachableUrl(targetUrl, opts)
+    timestamp = timestamp()
+    return {
+      ...pick(res, RESPONSE_PROPS),
+      redirectStatusCode: statusCode,
+      timestamp
+    }
+  } catch (aggregatedError) {
+    timestamp = timestamp()
+    const error = getLastError(aggregatedError)
+    const { url } = error
+
+    return {
+      url,
+      timestamp,
+      requestUrl: url,
+      redirectUrls: [],
+      statusCode: error.statusCode || dnsErrors(error).statusCode || 500
+    }
+  }
+}
 
 module.exports = async (urls, { concurrence = 8, ...opts } = {}) => {
   const emitter = mitt()
 
-  async function iterator (acc, url) {
-    emitter.emit('fetching', { url })
-
-    const end = timeSpan()
-    const statusCode = await getStatusCode(url, opts)
-    const timestamp = end()
-    const statusCodeGroup = getStatusByGroup(statusCode)
-    const data = { statusCodeGroup, statusCode, url, timestamp }
+  async function iterator (acc, targetUrl) {
+    emitter.emit('fetching', { url: targetUrl })
+    const res = await fetch(targetUrl, opts)
+    const statusCodeGroup = getStatusByGroup(
+      res.redirectStatusCode || res.statusCode
+    )
+    const data = { ...res, statusCodeGroup }
 
     emitter.emit('status', data)
-
     if (!acc[statusCodeGroup]) acc[statusCodeGroup] = [data]
     else acc[statusCodeGroup].push(data)
   }
--- test/index.js
+++ test/index.js
@@ -6,21 +6,62 @@ const test = require('ava')
 
 const urlint = require('..')
 
-test('Resolve an array of urls', async t => {
+const each = (data, fn) =>
+  forEach(data, groupByStatusCode => forEach(groupByStatusCode, fn))
+
+test('resolve an array of urls', async t => {
   const urls = ['https://kikobeats.com', 'https://microlink.io']
 
   const emitter = await urlint(urls)
   const data = await pEvent(emitter, 'end')
 
-  forEach(data, groupByStatusCode => {
-    forEach(
-      groupByStatusCode,
-      ({ statusCodeGroup, statusCode, url, timestamp }) => {
-        t.is(true, isString(statusCodeGroup))
-        t.is(true, isNumber(statusCode))
-        t.is(true, isString(url))
-        t.is(true, isNumber(timestamp))
-      }
-    )
+  each(data, ({ statusCodeGroup, statusCode, url, timestamp }) => {
+    t.true(isString(statusCodeGroup))
+    t.true(isNumber(statusCode))
+    t.true(isString(url))
+    t.true(isNumber(timestamp))
+  })
+})
+
+test('resolve DNS errors', async t => {
+  const urls = [
+    'http://android-app/com.twitter.android/twitter/user?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eandroidseo%7Ctwgr%5Eprofile&screen_name=Kikobeats'
+  ]
+  const emitter = await urlint(urls)
+  const data = await pEvent(emitter, 'end')
+
+  each(data, ({ statusCodeGroup, statusCode, url }) => {
+    t.is(statusCode, 404)
+    t.is(statusCodeGroup, '4xx')
+  })
+})
+
+test('follow redirects', async t => {
+  const urls = ['https://httpbin.org/redirect/6']
+  const emitter = await urlint(urls)
+  const data = await pEvent(emitter, 'end')
+
+  each(data, data => {
+    const {
+      redirectStatusCode,
+      requestUrl,
+      redirectUrls,
+      statusCodeGroup,
+      statusCode,
+      url
+    } = data
+    t.is(statusCode, 200)
+    t.is(redirectStatusCode, 302)
+    t.is(statusCodeGroup, '3xx')
+    t.is(requestUrl, 'https://httpbin.org/redirect/6')
+    t.is(url, 'https://httpbin.org/get')
+    t.deepEqual(redirectUrls, [
+      'https://httpbin.org/relative-redirect/5',
+      'https://httpbin.org/relative-redirect/4',
+      'https://httpbin.org/relative-redirect/3',
+      'https://httpbin.org/relative-redirect/2',
+      'https://httpbin.org/relative-redirect/1',
+      'https://httpbin.org/get'
+    ])
   })
 })
